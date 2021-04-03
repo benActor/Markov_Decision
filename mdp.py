17ca6dc58416588b9407a1413c89e9b1c1b79fd2
benActor <210335@university-365.com>
2021-04-04 00:26:23 +0200

    markov function added

---
+++ mdp.py
@@ -0,0 +1,37 @@
+from node import Node
+from action import Action
+
+class Mdp:
+    GAMMA = 0.6
+
+    CONV_FACTOR = 10e-9
+
+    @staticmethod
+    def Q(state, action, board, circular):
+        return {"action": action,
+                "value": sum(transition.prob * (transition.reward + Mdp.GAMMA * board[transition.end_pt].value)
+                             for transition in state.set_transitions(action, board, circular))}
+
+    @staticmethod
+    def markovDecision(layout, circle):
+        board = [Node(pos, layout[pos], 0) for pos in range(len(layout))]
+        board[0].n_type, board[-1].n_type = 0, 0
+        board[-1].value = 0
+        print(board)
+        while True:
+            for i in range(len(board) - 1):
+                state = board[i]
+                if state.pos == 14:
+                    state.value = 0
+                else:
+                    state.new_value = max(Mdp.Q(state, action, board, circle)["value"] for action in Action.ROLL)
+                    x = [Mdp.Q(state, action, board, circle) for action in Action.ROLL]
+                    for q in x:
+                        if q["value"] == state.new_value:
+                            state.optimal_action = q["action"]
+
+            if max(abs(state.value - state.new_value) for state in board) < Mdp.CONV_FACTOR:
+                return {"Expec": [node.new_value for node in board], "Dice": [node.optimal_action for node in board]}
+
+            for val in board:
+                val.value = val.new_value
