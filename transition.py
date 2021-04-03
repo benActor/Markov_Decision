9966472b221a1a20d8f546a88652fba7831af424
benActor <210335@university-365.com>
2021-04-03 18:01:43 +0200

    transition comparison and representation added

--- transition.py
+++ transition.py
@@ -1,3 +1,18 @@
 class Transition:
     def __init__(self, start_pt, end_pt, action, prob, reward):
         self.start_pt, self.end_pt, self.action, self.prob, self.reward = start_pt, end_pt, action, prob, reward
+
+    def __repr__(self):
+        return str({
+            "from": self.start_pt,
+            "to": self.end_pt,
+            "with_actions": f"{self.action}",
+            "prob": self.prob,
+            "reward": self.reward
+        })
+
+    def __eq__(self, other):
+        if not isinstance(other, Transition):
+            return NotImplemented
+        return self.start_pt == other.end_pt and \
+            self.action == other.action and self.end_pt == other.end_pt
