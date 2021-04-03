2b73dee3bfe644ab6807e64b36b254b8156e4386
benActor <210335@university-365.com>
2021-04-04 00:27:04 +0200

    running sample added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-
+from mdp import Mdp
 
 if __name__ == '__main__':
-    pass
+    from random import randint
+
+    layout = [randint(0, 4) for i in range(15)]
+    print(Mdp.markovDecision(layout, False))
