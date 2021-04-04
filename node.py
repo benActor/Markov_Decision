9ca3378458c84f03e9a1791a86321b5b0affb219
benActor <210335@university-365.com>
2021-04-04 20:23:07 +0200

    index issue on transitions corrected

--- node.py
+++ node.py
@@ -26,7 +26,7 @@ class Node:
         transitions = self.s_primes(d_type, board, circular)
         all_transitions = []
         if d_type != "risky_dice":
-            for tran in transitions[d_type]:
+            for tran in Node.rem_duplicate(transitions[d_type]):
 
                 # identical transitions from a point to the other
                 identical = list(filter(lambda s: s["initial_pos"] == tran["initial_pos"] and s["pos"] == tran["pos"],
@@ -37,7 +37,7 @@ class Node:
 
                 all_transitions.append(Transition(tran["initial_pos"], tran["pos"], d_type, prob, reward))
         else:
-            for tran in transitions[d_type]:
+            for tran in Node.rem_duplicate(transitions[d_type]):
 
                 # identical transitions from a point to the other
                 identical = list(filter(lambda s: s["initial_pos"] == tran["initial_pos"] and s["pos"] == tran["pos"],
@@ -51,20 +51,41 @@ class Node:
                     reward = Action.r(tran["initial_pos"], tran["pos"], self)
 
                 all_transitions.append(Transition(tran["initial_pos"], tran["pos"], d_type, prob, reward))
-        return Node.rem_dupli(all_transitions)
+        return all_transitions
 
     def sec_dice_s(self, d_type):
         if self.pos not in [2, 9]:
             return [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw,
                      "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]]
         if self.pos == 2:
-            return self.rem_dupli([{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw,
-                                    "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]] +
+            return [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw,
+                                    "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]] +\
                                   [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw * 8,
-                                    "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]])
+                                    "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]]
         if self.pos == 9:
             return [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + (draw * 5),
                      "prob": Action.prob(self, "security_dice", draw)} for draw in Action.ROLL[d_type]]
+
+    def new_set_penalty(self, via_node):
+        """
+        si le noeud suivant est de type piège on définit la valeur du piège
+
+        :return:
+        """
+        penalty = 0
+
+        if via_node.n_type == 1:
+            penalty = 0
+
+        if via_node.n_type == 2:
+            if via_node.pos in [10, 11, 12]:
+                penalty = via_node.pos - 10
+            else:
+                penalty = via_node.pos - 3 if via_node.pos > 2 else 0
+
+        # if prison stay to the same point
+        if via_node.n_type == 3:
+            penalty = via_node.pos
 
     def set_trap(self):
         penalty = 0
@@ -105,20 +126,16 @@ class Node:
                         s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw + 7,
                                "prob": Action.prob(self, "normal_dice", draw)}]
                     elif self.pos == 9:
-                        if not draw:
-                            s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos,
+                        if self.pos + draw > 9 and not circular:
+                            s += [{"initial_pos": self.pos, "draw": draw, "pos": 14,
                                    "prob": Action.prob(self, "normal_dice", draw)}]
                         else:
-                            if self.pos + draw > 9 and not circular:
-                                s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + 14,
+                            if draw == 1:
+                                s += [{"initial_pos": self.pos, "draw": draw, "pos": 14,
                                        "prob": Action.prob(self, "normal_dice", draw)}]
                             else:
-                                if draw == 1:
-                                    s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + 5,
-                                           "prob": Action.prob(self, "normal_dice", draw)}]
-                                else:
-                                    s += [{"initial_pos": self.pos, "draw": draw, "pos": 0,
-                                           "prob": Action.prob(self, "normal_dice", draw)}]
+                                s += [{"initial_pos": self.pos, "draw": draw, "pos": 0,
+                                       "prob": Action.prob(self, "normal_dice", draw)}]
                     elif self.pos in [12, 13]:
                         if self.pos + draw <= 14:
                             s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + draw,
@@ -130,8 +147,6 @@ class Node:
                             else:
                                 s += [{"initial_pos": self.pos, "draw": draw, "pos": 15 - self.pos +draw,
                                        "prob": Action.prob(self, "normal_dice", draw)}]
-
-
             return s
 
         elif self.n_type == 0:
@@ -203,6 +218,9 @@ class Node:
                     if draw:
                         s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos + 7 + draw,
                                "prob": Action.prob(self, "normal_dice", draw)}]
+                    else:
+                        s += [{"initial_pos": self.pos, "draw": draw, "pos": penalty,
+                               "prob": Action.prob(self, "normal_dice", draw)}]
                 return s
 
             if self.pos == 9:
@@ -239,7 +257,6 @@ class Node:
                                    "prob": Action.prob(self, "normal_dice", draw)}]
                 return s
 
-
     def set_dest_trap(self, via_node):
         penalty = 0
         # if restart penalty brings to pos 0
@@ -263,14 +280,14 @@ class Node:
         s = []
         if via_node.n_type == 4:
             s += [{"initial_pos": self.pos, "draw": draw, "pos": i, "via": via_node.pos,
-                   "prob": Action.prob(via_node, "risky_dice", draw)/15} for i in range(15)]
+                   "prob": Action.prob(self, "risky_dice", draw)/15} for i in range(15)]
         elif via_node.n_type == 0:
             s += [{"initial_pos": self.pos, "draw": draw, "pos": via_node.pos, "via": None,
-                   "prob": Action.prob(via_node, "risky_dice", draw)}]
+                   "prob": Action.prob(self, "risky_dice", draw)}]
         else:
             penalty = self.set_dest_trap(via_node)
             s += [{"initial_pos": self.pos, "draw": draw, "pos": penalty, "via": via_node.pos,
-                   "prob": Action.prob(via_node, "risky_dice", draw)}]
+                   "prob": Action.prob(self, "risky_dice", draw)}]
 
         return s
 
@@ -288,6 +305,7 @@ class Node:
                 if not draw:
                     via_node = self
                     s += self.fill_state(via_node, draw)
+                    s += self.fill_state(via_node, draw)
                 else:
                     for via_node in [board[self.pos + draw], board[self.pos + draw + 7]]:
                         s += self.fill_state(via_node, draw)
@@ -297,13 +315,13 @@ class Node:
             s = []
             for draw in Action.ROLL[d_type]:
                 if not draw:
-                    via_node = self
-                    s += self.fill_state(via_node, draw)
+                    s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos, "via": None,
+                           "prob": Action.prob(self, "risky_dice", draw)}]
                 else:
                     if self.pos + draw <= 9:
                         via_node = board[self.pos + draw]
                         s += self.fill_state(via_node, draw)
-                    if self.pos + draw > 9 and not circular:
+                    elif self.pos + draw > 9 and not circular:
                         s += [{"initial_pos": self.pos, "draw": draw, "pos": 14, "via": None, "prob": Action.prob(self, "risky_dice", draw)}]
                     else:
                         if self.pos + draw + 4 == 14:
@@ -316,17 +334,20 @@ class Node:
         if self.pos in [12, 13]:
             s = []
             for draw in Action.ROLL[d_type]:
-                if self.pos + draw <= 14:
-                    via_node = board[self.pos + draw]
-                    s += self.fill_state(via_node, draw)
-                elif self.pos + draw > 14 and not circular:
-                    via_node = board[14]
-                    s += self.fill_state(via_node, draw)
+                if not draw:
+                    s += [{"initial_pos": self.pos, "draw": draw, "pos": self.pos, "via": None,
+                           "prob": Action.prob(self, "risky_dice", draw)}]
                 else:
-                    via_node = board[self.pos + draw - 15]
-                    s += self.fill_state(via_node, draw)
+                    if self.pos + draw <= 14:
+                        via_node = board[self.pos + draw]
+                        s += self.fill_state(via_node, draw)
+                    elif self.pos + draw > 14 and not circular:
+                        via_node = board[14]
+                        s += self.fill_state(via_node, draw)
+                    else:
+                        via_node = board[self.pos + draw - 15]
+                        s += self.fill_state(via_node, draw)
             return s
-
 
     def s_primes(self, d_type, board, circular=False):
         if d_type == "security_dice":
@@ -339,14 +360,44 @@ class Node:
             return {d_type: self.risky_dice_s(d_type, board, circular)}
 
     @staticmethod
-    def rem_dupli(trans_list):
+    def equal(a, b):
+        return a["initial_pos"] == b["initial_pos"] and a["pos"] == b["pos"]
+
+
+    @staticmethod
+    def a_in_l(l, a):
+        if not l:
+            return False
+        elif Node.equal(a, l[0]):
+            return True
+        else:
+            return Node.a_in_l(l[1:], a)
+
+    @staticmethod
+    def combine_prob(similar_trans):
+        return {"initial_pos": similar_trans[0]["initial_pos"],
+                "draw": similar_trans[0]["draw"],
+                "pos": similar_trans[0]["pos"],
+                "prob": sum(trans["prob"] for trans in similar_trans)}
+
+    @staticmethod
+    def gsm(l, elt):
         dupli = []
         unique = []
-        for i in range(len(trans_list)):
-            if trans_list[i] in trans_list[i + 1:] or trans_list[i] in dupli:
-                dupli.append(trans_list[i])
+        for i in l:
+            if Node.equal(i, elt):
+                dupli.append(i)
             else:
-                unique.append(trans_list[i])
+                unique.append(i)
         if dupli:
-            return [dupli[0]] + unique
+            unique.append(Node.combine_prob(dupli))
         return unique
+
+
+    @staticmethod
+    def rem_duplicate(tran_list):
+        res = Node.gsm(tran_list, tran_list[0])
+        for x in tran_list[1:]:
+            res = Node.gsm(res, x)
+        return res
+
