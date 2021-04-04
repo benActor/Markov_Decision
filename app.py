62949088d27ad05eb338f22501683a7e7d670496
benActor <210335@university-365.com>
2021-04-04 20:57:21 +0200

    layout converted from list to numpy array

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from mdp import Mdp
+import  numpy
 
 if __name__ == '__main__':
     from random import randint
-
-    layout = [randint(0, 4) for i in range(15)]
-    print(Mdp.markovDecision(layout, False))
+    layout = numpy.array([randint(0, 4) for i in range(15)])
+    print(Mdp.markovDecision(layout, True))
