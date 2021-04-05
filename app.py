8d4120a6c29e47ee43daf0cc14a1d4c0de431acf
benActor <210335@university-365.com>
2021-04-05 19:51:57 +0200

    running simulation

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ import numpy
 if __name__ == '__main__':
     from random import randint
     layout = numpy.array([randint(0, 4) for i in range(15)])
-    print(Mdp.markovDecision(layout, True))
+
+    optimal_policy = Mdp.markovDecision(layout, True)[1]
+
+    print(Simulation.play(layout, optimal_policy, True))
