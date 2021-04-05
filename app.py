a85be16d7eb8e787c657819778f856f0e6bf00c7
benActor <34540775+benActor@users.noreply.github.com>
2021-04-05 19:48:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     from random import randint
     layout = numpy.array([randint(0, 4) for i in range(15)])
     print(Mdp.markovDecision(layout, True))
+    
+    print(Simulation.play(layout, Mdp.markovDecision(layout, True), True))
